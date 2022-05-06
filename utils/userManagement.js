ce39918ce6e1fd1e479092470133f72ba7bff020
judeakinwale <judeakinwale@gmail.com>
2022-05-06 11:06:36 +0100

    Improve configure head roles util

--- utils/userManagement.js
+++ utils/userManagement.js
@@ -7,21 +7,25 @@ exports.configureHeadRoles = asyncHandler(async (team, req, res, next) => {
   /**
    * Configure Procurement Head (HOP) and Team Head Roles when roles are created or updated
    * */ 
-  if (team.title == "Procurement" && team.head) {
-    const procurementHead = await Staff.findById(team.head)
-    const procurementHeadRole = await Role.findOne({title: "HOP"})
-    procurementHead.isTeamHead = true
-    procurementHead.isHOP = true
-    procurementHead.role = procurementHeadRole
-    await procurementHead.save()
-
-  } else if (team.head) {
-    const head = await Staff.findById(team.head)
-    const headRole = await Role.findOne({title: "Team Head"})
-    head.isTeamHead = true
-    head.role = headRole
-    await head.save()
+  try {
+    if (team.title == "Procurement" && team.head) {
+      const procurementHead = await Staff.findById(team.head)
+      const procurementHeadRole = await Role.findOne({title: "HOP"})
+      procurementHead.isTeamHead = true
+      procurementHead.isHOP = true
+      procurementHead.role = procurementHeadRole
+      await procurementHead.save()
+  
+    } else if (team.head) {
+      const head = await Staff.findById(team.head)
+      const headRole = await Role.findOne({title: "Team Head"})
+      head.isTeamHead = true
+      head.role = headRole
+      await head.save()
+    }
+    return true
+  } catch (err) {
+    return false
   }
 
-  return true
 })
