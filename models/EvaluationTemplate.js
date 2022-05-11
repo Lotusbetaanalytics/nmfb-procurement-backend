02b5236ad79a1ec28455380a0a84faea5a3d7489
judeakinwale <judeakinwale@gmail.com>
2022-05-11 07:43:44 +0100

    Update evaluation template and project task models

--- models/EvaluationTemplate.js
+++ models/EvaluationTemplate.js
@@ -8,42 +8,14 @@ const EvaluationTemplateSchema = new mongoose.Schema({
   employeeEmail: {
     type: String,
   },
-  contract: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ContractType"
-  }, // Not sure how to Implement
-  project: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ProjectInitiation"
-  },
-  projectTitle: {
+  question: {
     type: String,
   },
-  projectType: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ProjectType"
-  },
-  evaluatingOfficer: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Staff"
-  },
-  businessUsersUnitName: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Team"
-  },
-  validFor: {
+  answerType: {
     type: String,
   },
-  comment: {
+  answer: {
     type: String,
-  },
-  files: {
-    type: Array,
-  },
-  status: {
-    type: String,
-    enum: ["Pending", "Started", "Completed"],
-    default: "pending",
   },
   createdAt: {
     type: Date,
--- models/ProjectTask.js
+++ models/ProjectTask.js
@@ -1,63 +1,63 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const ProjectOnboardingSchema = new mongoose.Schema({
+const ProjectTaskSchema = new mongoose.Schema({
 
-//   name: {
-//     type: String,
-//   },
-//   email: {
-//     type: String,
-//   },
-//   project: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "ProjectInitiation"
-//   },
-//   projectTitle: {
-//     type: String,
-//   },
-//   assignedBy: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Staff"
-//   },
-//   assignedTo: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Staff"
-//   },
-//   reassignedTo: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Staff"
-//   },
-//   responsibleOfficer: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Staff"
-//   },
-//   responsibleUnit: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Team"
-//   },
-//   comment: {
-//     type: String,
-//   },
-//   files: {
-//     type: Array,
-//   },
-//   status: {
-//     type: String,
-//     enum: ["Pending", "Started", "Reassigned", "On Hold", "Terminated", "Completed"],
-//     default: "pending",
-//   },
-//   isApproved: {
-//     type: Boolean,
-//     default: false,
-//   },
-//   createdBy: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Staff"
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now(),
-//   },
-// });
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ProjectInitiation"
+  },
+  projectTitle: {
+    type: String,
+  },
+  assignedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Staff"
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Staff"
+  },
+  reassignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Staff"
+  },
+  responsibleOfficer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Staff"
+  },
+  responsibleUnit: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Team"
+  },
+  comment: {
+    type: String,
+  },
+  files: {
+    type: Array,
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Started", "Reassigned", "On Hold", "Terminated", "Completed"],
+    default: "pending",
+  },
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Staff"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-// module.exports = mongoose.model("ProjectOnboarding", ProjectOnboardingSchema);
+module.exports = mongoose.model("ProjectTask", ProjectTaskSchema);
