8092843f5008e1d676a370a6e650ece5b07fa2d8
judeakinwale <judeakinwale@gmail.com>
2022-05-18 10:54:26 +0100

    Rearrange contract and staff routes

--- routes/contract.js
+++ routes/contract.js
@@ -16,12 +16,12 @@ const advancedResults = require("../middleware/advancedResults");
 
 router.post("/", verifyToken, createContract); // create a contract
 router.get("/", advancedResults(Contract), getAllContracts); // get all contracts
+router.get("/active", verifyToken, getAllActiveContracts); // get all active contracts
+router.get("/terminated", verifyToken, getAllTerminatedContracts); // get all terminated contracts
+router.get("/failed", verifyToken, getAllFailedContracts); // get all failed contracts
 router.get("/:id", verifyToken, getContract); // get contract details by id
 router.patch("/:id", verifyToken, updateContract); // update contract details by id
 router.delete("/:id", verifyToken, deleteContract); // delete contract by id
 router.get("/:id/terminate", verifyToken, terminateContract); // create a contract
-router.get("/active", verifyToken, getAllActiveContracts); // create a contract
-router.get("/terminated", verifyToken, getAllTerminatedContracts); // create a contract
-router.get("/failed", verifyToken, getAllFailedContracts); // create a contract
 
 module.exports = router;
--- routes/staff.js
+++ routes/staff.js
@@ -18,13 +18,13 @@ const advancedResults = require("../middleware/advancedResults");
 
 router.post("/", createStaff); // create a staff
 router.get("/", advancedResults(Staff), getAllStaffs); // get all staffs
+router.get("/teamHead", verifyToken, getTeamHeads); // get all team heads
+router.get("/projectDeskOfficer", verifyToken, getPDOs); // get all project desk officers
+router.get("/frontDesk", verifyToken, getAdmins); // get all front desk officers
+router.get("/headOfProcurement", verifyToken, getHOP); // get head of procurement
 router.get("/:id", verifyToken, getStaff); // get staff details by id
 router.patch("/:id", verifyToken, updateStaff); // update staff details by id
 router.delete("/:id", verifyToken, deleteStaff); // delete staff by id
-router.get("/teamHead", verifyToken, getTeamHeads); // get all team heads
-router.get("/PDO", verifyToken, getPDOs); // get all project desk officers
-router.get("/frontDesk", verifyToken, getAdmins); // get all front desk officers
-router.get("/headOfProcurement", verifyToken, getHOP); // get head of procurement
 router.get("/team/:id", verifyToken, getTeamStaff); // get staff details by team id
 router.get("/role/:id", verifyToken, getRoleStaff); // get staff details by role id
 
